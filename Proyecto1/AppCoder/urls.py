81a5eb9958e588c15512a5ab2f10232f1b9c4de3
RomanValverde <romanvalverde@hotmail.com.ar>
2023-06-26 00:40:43 -0300

    Realizamos nuestra home page con el NAV, realizamos herencia de HTML y agregamos nuevas templates con posibilidad de guardar estudiantes y de buscar a los mismos mediante formularios.

--- Proyecto1/AppCoder/urls.py
+++ Proyecto1/AppCoder/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from AppCoder.views import *
 
 urlpatterns = [
-    path('inicio/', inicio),
+    path('inicio/', inicio, name="Inicio"),
     path('curso/', curso, name="Curso"),
-    path('profesor/', profesor, name="Profesores")
+    path('profesor/', profesor, name="Profesores"),
     path('estudiante/', estudiante, name="Estudiantes"),
     path('entregable/', entregable, name='Entregables'),    
     path('setEstudiante/',setEstudiantes, name="setEstudiante"),
+    path('getEstudiante/',getEstudiantes, name="getEstudiante"),
+    path('buscarEstudiante/',buscarEstudiantes, name="buscarEstudiante"),
 ]
--- Proyecto1/AppCoder/views.py
+++ Proyecto1/AppCoder/views.py
@@ -28,15 +28,30 @@ def setEstudiantes(request):
         print(miFormulario)
         if miFormulario.is_valid:
             data = miFormulario.cleaned_data
-            estudiante = Estudiante(nombre = data["nombre"], apellido = data["apellido"],email = data["email"])
+            estudiante = Estudiante(nombre = data["nombre"], apellido = data["apellido"], email = data["email"])
             estudiante.save()
-        return render(request,"AppCoder/inicio.html")
+            return render(request,"AppCoder/inicio.html")
     else:
-        return render(request,"AppCoder/setEstudiantes.html")   
+        miFormulario = formSetEstudiante()
 
-    if request.method == 'POST':
+    return render(request,"AppCoder/setEstudiantes.html", {"miFormulario" : miFormulario})   
+
+    """ METODO VIEJO
+      if request.method == 'POST':
         estudiante = Estudiante(nombre = request.POST["nombre"], apellido = request.POST["apellido"],email = request.POST["email"])
         estudiante.save()
         return render(request,"AppCoder/inicio.html")
-    return render(request,"AppCoder/setEstudiantes.html")
+    return render(request,"AppCoder/setEstudiantes.html") """
 
+def getEstudiantes(request):
+    return render(request,"AppCoder/getEstudiantes.html")
+
+def buscarEstudiantes(request):
+    if request.GET["nombre"]:
+        nombre = request.GET["nombre"]
+        estudiantes = Estudiante.objects.filter(nombre = nombre)
+        return render(request, "AppCoder/getEstudiantes.html", {"estudiantes": estudiantes})
+    else:
+        respuesta = "No se enviaron datos"
+    
+    return HttpResponse(respuesta)
